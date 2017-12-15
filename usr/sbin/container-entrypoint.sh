cbaf81b6896c8da4756a5d8af19961f7eefbeac1
Egor Zyuskin <ezyuskin@codenetix.com>
2017-12-15 14:00:43 +0600

    Add SET_HTTP env varible

--- usr/sbin/container-entrypoint.sh
+++ usr/sbin/container-entrypoint.sh
@@ -6,9 +6,13 @@ for VH in `env | grep "VH_"`; do
     ENV=`echo $VH | awk -F "=" '{print $1}'`
     HOST=`echo $ENV | awk -F "_" '{print $2}'`
     TO_HOST=`echo $VH | awk -F "=" '{print $2}'`
-    echo "server {listen 80; server_name $HOST; location / { proxy_pass $TO_HOST; } }" > /etc/nginx/conf.d/$HOST.conf
+    echo "server {listen 80; server_name $HOST; location / { proxy_pass $TO_HOST; } }" > /etc/nginx/conf.d/host-$HOST.conf
 done;
 
-echo "`env`" > /tmp/12312.txt
+for SET_HTTP in `env | grep "SET_HTTP_"`; do
+    ENV=`echo $SET_HTTP | cut -d'=' -f 1'`
+    VALUE=`echo $SET_HTTP | cut -d'=' -f2`
+    echo "${ENV:9} $VALUE;" >> /etc/nginx/conf.d/http_vars.conf
+done;
 
 exec "$@"
